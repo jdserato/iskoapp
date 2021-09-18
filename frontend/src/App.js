de2ec6ab396c5ed834d76eb9e4ac29eae039fa73
Neil Axinto <naxinto@proweaver.net>
2021-09-18 09:37:40 +0800

    added Watch for both new and returning users

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Landing from './pages/landing/Landing'
 import Signup from './pages/signup/Signup'
 import Login from './pages/login/Login'
 import Home from './pages/home/Home'
+import Watch from './pages/watch/Watch'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="/signup" exact component={Signup} />
           <Route path="/login" exact component={Login} />
           <Route path="/home" exact component={Home} />
+          <Route path="/watch" exact component={Watch} />
         </Switch>
       </div>
     </Router>
---
+++ frontend/src/pages/watch/Watch.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react'
+import './Watch.css'
+import Header from '../../components/header/Header'
+import VideoBox from '../../components/videoBox/VideoBox'
+import ZeroPurchases from '../../images/zeropurchases.png'
+
+class watch extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             purchasedCount: 0
+        }
+    }
+    
+    render() {
+        // Sample content object for VideoBox component
+        var content = {
+            /* uncomment this for image URL */
+            //imageUrl: "",
+            isPlaylist: false,
+            price: "PHP 15.00",
+            totalVideos: "10 videos",
+            rating: 4,
+            title: "Course Title",
+            description: "Let’s add a short course description here which should be max two liner only I guess..."
+        }
+        var playlistContent = {
+            /* uncomment this for image URL */
+            //imageUrl: "",
+            isPlaylist: true,
+            price: "PHP 15.00",
+            totalVideos: "10 videos",
+            rating: 4,
+            title: "Course Title",
+            description: "Let’s add a short course description here which should be max two liner only I guess..."
+        }
+
+        let body
+        if(this.state.purchasedCount === 0){
+            body = 
+            <div id="watch-body" className="watch-body-new-user">
+                <h1>Welcome back, Charlene!</h1>
+                <figure>
+                    <img src={ZeroPurchases} alt="zero purchases" />
+                </figure>
+                <p>You haven’t made any purchases yet. Try your luck at the shop!</p>
+                <a href="#!">SHOP NOW!</a>
+            </div>
+        }
+        else{
+            body = 
+                <div id="watch-body">
+                    <h1>Welcome back, Charlene!</h1>
+
+                    <div className="video-list">
+                        <h2>Recently Purchased <a href="#!">See All</a></h2>
+                        <div className="video-boxes">
+                            <section>
+                                <VideoBox content = {content} />
+                            </section>
+                            <section>
+                                <VideoBox content = {content} />
+                            </section>
+                            <section>
+                                <VideoBox content = {content} />
+                            </section>
+                        </div>
+                    </div>
+
+                    <div className="video-list">
+                        <h2>Continue Watching <a href="#!">See All</a></h2>
+                        <div className="video-boxes">
+                            <section>
+                                <VideoBox content = {content} />
+                            </section>
+                        </div>
+                    </div>
+
+                    <div className="video-list playlist">
+                        <h2>Recently Playlist <a href="#!">See All</a></h2>
+                        <div className="video-boxes">
+                            <section>
+                                <VideoBox content = {playlistContent} />
+                            </section>
+                        </div>
+                    </div>
+
+                </div>
+        }
+        return (
+            <div>
+            <Header isLoggedIn={true} activeList={2}/>
+                <div className="wrapper">
+                    {body}
+                </div>
+            
+            </div>
+        )
+    }
+}
+
+export default watch
