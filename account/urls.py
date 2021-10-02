fb6d4366032bab4fab3c10ac0de91f440b4cdf9f
Neil Axinto <naxinto@proweaver.net>
2021-10-03 06:37:57 +0800

    test

--- account/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('api/account/', views.AccountListCreate.as_view() ),
-    path('api/auth/login', views.LoginAPI.as_view() ),
-    path('api/auth/user', views.UserAPI.as_view()),
-]
